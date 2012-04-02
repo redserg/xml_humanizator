9e0dd0c60254d0ef298452087205229b2e820a0c
spa <spa@alpha.ru>
2012-04-02 15:03:18 +0400

    added header(or title) and some formats

--- main.pl
+++ main.pl
@@ -7,7 +7,9 @@ use CGI ;
 
 my $q = CGI->new;
 print $q->header,
-	$q->start_html("Conver XML to YAML");
+	$q->start_html('Convert XML to YAML');
+print $q->h1({-align=>"left"},'Convert XML to YAML');
+print $q->p,$q->defaults;
 print_textpart();
 print_filepart();
 process_textpart();
@@ -16,13 +18,12 @@ print $q->end_html;
 
 sub print_textpart{
 	print $q->start_form;
-	print $q->h1("TEXT");
+	print $q->h3('TEXT');
 	print $q->textarea(-name=>'textarea_input',
-		-rows=>10,
-		-columns=>50);
+		-rows=>5,
+		-columns=>25);
 
-	print $q->p,$q->defaults;
-	print $q->submit(-name=>'text_button',
+	print $q->p,$q->submit(-name=>'text_button',
                          -value=>'convert');
 	print $q->end_form;
 	print $q->hr;
@@ -30,18 +31,19 @@ sub print_textpart{
 
 sub print_filepart{
 	print $q->start_form;
-	print $q->h1("FILE");
+	print $q->h3("FILE");
 	print $q->filefield(-name=>'uploaded_file',
                             -default=>'starting value',
                             -size=>50,
-                            -maxlength=>80);
-	print $q->submit(-name=>'file_button',
+                            -maxlength=>80),
+          $q->submit(-name=>'file_button',
                     -value=>'process file');
 	print $q->end_form;
 	print $q->hr;
 }
 sub process_textpart {	
 	if($q->param('text_button')){
+		print $q->h3('processed text');
 		print $q->param('textarea_input');
 		print $q->hr;
 		# print YAML::Dump(XML::Simple::XMLin($q->param("textarea_input")));
@@ -50,8 +52,6 @@ sub process_textpart {
 
 sub process_filepart{	
 	if($q->param('file_button')){
-		
-
 		my $lightweight_fh  = $q->upload('uploaded_file');
 
 		if (defined $lightweight_fh) {
@@ -64,7 +64,7 @@ sub process_filepart{
     		while ($bytesread = $io_handle->read($buffer,1024)) {
     			$file_as_string.=$buffer;
  	   		}
- 	   		print $q->h1('processed file');
+ 	   		print $q->h3('processed file');
 			print $q->param('uploaded_file'), $q->p;
  	   		# print YAML::Dump(XML::Simple::XMLin($file_as_string));
  	   		print $file_as_string;
